eb15008b85986734ac24cdc7f294c1838b784065
Seotjuu <79149336+tjrenffl8@users.noreply.github.com>
2021-11-05 20:17:25 +0900

    [Update]

---
+++ Login_js.js
@@ -0,0 +1,11 @@
+function check(form)
+{
+    if(form.id.value=="tjrenffl8" && form.password.value=="1234")
+    {
+        alert("로그인 완료!!")
+    }
+    else
+    {
+        alert("로그인 실패!!")
+    }
+}

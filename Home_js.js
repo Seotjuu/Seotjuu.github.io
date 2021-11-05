dcd7668c93e6b89a760c466c4dbf96ecea08aa65
Seotjuu <79149336+tjrenffl8@users.noreply.github.com>
2021-11-06 00:40:02 +0900

    [Update]

--- Home_js.js
+++ Home_js.js
@@ -1,11 +1,1 @@
-function check(form)
-{
-    if(form.id.value=="tjrenffl8" && form.password.value=="1234")
-    {
-        alert("로그인 완료!!")
-    }
-    else
-    {
-        alert("로그인 실패!!")
-    }
-}
+adasdd
--- Login_js.js
+++ Login_js.js
@@ -2,7 +2,8 @@ function check(form)
 {
     if(form.id.value=="tjrenffl8" && form.password.value=="1234")
     {
-        alert("로그인 완료!!")
+        alert("로그인 완료!!");
+        window.location.href = "Home.html";
     }
     else
     {

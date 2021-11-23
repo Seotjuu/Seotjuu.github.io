36ea6bc007a345c72ec0e193ec5d4250b41b664f
unknown <kjunhun9@sogang.ac.kr>
2021-11-23 21:05:04 +0900

    211123

--- Login.js
+++ Login.js
@@ -1,33 +1,37 @@
-
-sessionStorage.setItem("seotju123", "1234");
-console.log(sessionStorage);
-
 var a = "seotju123";
-var b = sessionStorage.seotju123;
-
+var b = '1234';
 
 function check() {
-    if(id.value == "" && pw.value == ""){
+    if (id.value == "" && pw.value == "") {
         alert("로그인 정보를 입력해주세요 !!");
-    }
-
-    else if(pw.value == ""){
+    } else if (pw.value == "") {
         alert("비밀번호를 입력해주세요 !!");
         location.reload;
-    }
-
-    else if(id.value == ""){
+    } else if (id.value == "") {
         alert("아이디를 입력해주세요 !!");
         location.reload;
-    }
-    
-    else if(id.value == a && pw.value == b){
+    } else if (id.value == a && pw.value == b) {
         alert("로그인 성공 !!");
-        location.href = 'index.html';
-        
-    }
-
-    else{
+        sessionStorage.id = a, sessionStorage.pw = b;
+        console.log(sessionStorage);
+        location.reload();
+    } else {
         alert("아이디 및 비밀번호를 다시 입력해주세요 !!");
+    }
+}
+
+_wresize();
+
+function _wresize() {
+    if (window.outerWidth < 600) {
+        document.querySelector('.Header-box').classList.add('mobile-header');
+        document.querySelector('.Footer-box').classList.add('mobile');
+        document.querySelector('.Header-A').classList.add('mobile');
+        document.querySelector('.Header-B').classList.add('mobile');
+    } else {
+        document.querySelector('.Header-box').classList.remove('mobile-header');
+        document.querySelector('.Footer-box').classList.remove('mobile');
+        document.querySelector('.Header-A').classList.remove('mobile');
+        document.querySelector('.Header-B').classList.remove('mobile');
     }
 }
